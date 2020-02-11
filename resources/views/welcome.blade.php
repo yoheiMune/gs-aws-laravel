6a6c5e343d25631344afe339af040bb81907931c
yoheiMune <y.munesada@gmail.com>
2020-02-11 14:10:47 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gs Lecture - Laravel × AWS
                 </div>
 
                 <div class="links">
