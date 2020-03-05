329b41853822eefe750b3da688eb3e0617ce8566
yoheiMune <y.munesada@gmail.com>
2020-03-05 15:26:44 +0900

    refactoring.

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Book;
-use App\Photo;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class HealthCheckController extends Controller
 {
